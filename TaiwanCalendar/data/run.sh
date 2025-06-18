79a08cc1f3cc88a2579e3ec484b3b2aa243b6ade
Ricky Su <ricky@ez2.us>
2025-06-18 15:45:47 +0800

    mount share folder

--- TaiwanCalendar/data/run.sh
+++ TaiwanCalendar/data/run.sh
@@ -2,7 +2,9 @@
 # shellcheck shell=bash
 set -e
 
-while true
-do
-  sleep 3600
-done
+PORT=$(bashio::config 'port')
+CACHE_BASE=$(bashio::config 'cache_base')
+
+mkdir -p "$CACHE_BASE"
+cd /script
+./index.js --port=$PORT --cacheBase="$CACHE_BASE"
