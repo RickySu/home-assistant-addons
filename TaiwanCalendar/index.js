ebea6fa3559e8c9a85d5d22cf41181b5dde67dab
Ricky Su <ricky@ez2.us>
2025-06-18 17:20:07 +0800

    add Taiwan Calendar Service

--- TaiwanCalendar/index.js
+++ TaiwanCalendar/index.js
@@ -17,7 +17,7 @@ const handleServer = () => {
     const date = new Date()
     const year = date.getFullYear()
     const month =  String(date.getMonth() + 1).padStart(2, '0')
-    const day =  String(date.getDate() + 1).padStart(2, '0')
+    const day =  String(date.getDate()).padStart(2, '0')
     if(calendarYear != year) {
       await fetchAllCalendar(year)
     }
